10346c7fd0bffdea37ad1ab017e71a6fe9cf0366
mcguirer <mcguirer@fake.fake>
2013-07-12 04:34:12 +0000

    playing

---
+++ ex19_2.py
@@ -0,0 +1,15 @@
+def ryans(arg1, arg2):
+	print "arg1: %r" % arg1
+	print "arg2: %r\n" % arg2
+
+ryans("foo", "bar")
+
+r1 = "goo"
+r2 = "bar"
+ryans(r1, r2)
+
+ryans(raw_input(), raw_input())
+
+print 
+
+
